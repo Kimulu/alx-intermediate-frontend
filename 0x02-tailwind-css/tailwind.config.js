62af33e0052d854ee1ec8e77bd88c4e9ebbd70cc
kimulu <kimulumichael@gmail.com>
2025-05-12 17:33:43 +0300

    add 1-index.html

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}", // For any HTML or JS inside the src folder
+    "./*.html", // For any HTML file directly in the root (e.g., 1-index.html)
+  ],
   theme: {
     extend: {},
   },
